8a5e262bed700f61964f55a519a7608ad75f603e
VinoodDindikurti <VinoodDindikurti@9.199.35.162>
2019-10-24 18:01:32 +0530

    test cases has been done

--- final-framework-testng/tests/com/training/sanity/tests/UniformadminloginUNF_016.java
+++ final-framework-testng/tests/com/training/sanity/tests/UniformadminloginUNF_016.java
@@ -61,7 +61,7 @@ public class UniformadminloginUNF_016 {
 	ordersPOM.clickcheckbox();
 	ordersPOM.delete();
 	//ordersPOM.alertaccept(); // Delete the selected order
-	ordersPOM.alertdismiss();  // cancel the selected orders for deletion
+	ordersPOM.alertdismiss();  // Cancel the selected orders for deletion
   }
 	
 	@AfterMethod
